fb59ba55ca69410ca4abc60d05022f16b339b5a7
Danaid Sinani <danaid@bu.edu>
2026-03-04 18:41:48 -0500

    fix the overflow issue

--- mp-3/src/components/Nav.tsx
+++ mp-3/src/components/Nav.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const StyledNav = styled.nav`
     width: 30%;
     background-color: #249E94;
+    overflow: hidden;
     @media screen and (max-width: 1000px) {
         width: 100%;
     }
@@ -11,10 +12,12 @@ const StyledNav = styled.nav`
 
 const StyledList = styled.ul`
     padding-left: 0;
+    margin: 0;
     list-style: none;
     @media screen and (max-width: 750px) {
         display: flex;
         flex-direction: row;
+        flex-wrap: wrap;
         justify-content: space-evenly;
     }
 `;
@@ -23,8 +26,10 @@ const StyledListItem = styled.li`
     border: 0.5vw solid #0C7779;
     width: 80%;
     margin: 5% auto;
+    overflow: hidden;
+    text-overflow: ellipsis;
     @media screen and (max-width: 750px) {
-        width: 18%;
+        width: 14%;
         padding: 1%;
         margin: 1%;
     }
@@ -34,6 +39,10 @@ const StyledLink = styled(Link)`
     font-size: calc(2px + 2vw);
     color: #005461;
     text-decoration: none;
+    white-space: nowrap;
+    @media screen and (max-width: 750px) {
+        font-size: calc(2px + 1.5vw);
+    }
 `;
 
 export default function Nav() {
